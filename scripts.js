c4702512bb9c307059948b512b135fda9fa0b83b
Cedric Hicks <chicks@wustl.edu>
2021-03-29 22:48:42 -0500

    Confirm box is working.

--- scripts.js
+++ scripts.js
@@ -1,2 +1,10 @@
 // Write your JavaScript code here.
 // Remember to pay attention to page loading!
+window.addEventListener("load", function() {
+    console.log('window loaded');
+
+    let launch = document.getElementById("takeoff");
+    launch.addEventListener("click", function () {
+        confirm("Confirm that the shuttle is ready for takeoff.");
+    }); 
+});
