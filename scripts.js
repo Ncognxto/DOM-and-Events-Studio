b568f7ab043329d156b488d96c244778b911c37c
Cedric Hicks <chicks@wustl.edu>
2021-03-30 21:10:04 -0500

    Added takeoff button changes.

--- scripts.js
+++ scripts.js
@@ -1,10 +1,17 @@
 // Write your JavaScript code here.
 // Remember to pay attention to page loading!
-window.addEventListener("load", function() {
-    console.log('window loaded');
+window.addEventListener("load", function(){
+    let takeOffButton = document.getElementById("takeoff");
+    let status = document.getElementById("flightStatus");
+    let background = document.getElementById("shuttleBackground");
+    let shuttleHeight = document.getElementById("spaceShuttleHeight");
 
-    let launch = document.getElementById("takeoff");
-    launch.addEventListener("click", function () {
-        confirm("Confirm that the shuttle is ready for takeoff.");
-    }); 
+    takeOffButton.addEventListener("click", function(){
+        if(confirm("Confirm that the shuttle is ready for takeoff.") == true) {
+            status.innerHTML = "Shuttle in flight."; 
+            background.style.backgroundColor = "blue";
+            shuttleHeight.innerHTML += "10000";
+        }
+    });
+
 });
