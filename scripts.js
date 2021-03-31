007d2876096193643f102a2a05df1c302e4fce5b
Cedric Hicks <chicks@wustl.edu>
2021-03-30 21:27:23 -0500

    Added land button changes.

--- scripts.js
+++ scripts.js
@@ -5,13 +5,23 @@ window.addEventListener("load", function(){
     let status = document.getElementById("flightStatus");
     let background = document.getElementById("shuttleBackground");
     let shuttleHeight = document.getElementById("spaceShuttleHeight");
+    let landButton = document.getElementById("landing");
+    let abortButton = document.getElementById("missionAbort");
 
     takeOffButton.addEventListener("click", function(){
         if(confirm("Confirm that the shuttle is ready for takeoff.") == true) {
             status.innerHTML = "Shuttle in flight."; 
             background.style.backgroundColor = "blue";
-            shuttleHeight.innerHTML += "10000";
+            shuttleHeight.innerHTML = "10000";
         }
-    });
+    })
+
+    landButton.addEventListener("click", function(){
+        alert("The shuttle is landing. Landing gear engaged.");
+        status.innerHTML = "The shuttle has landed.";
+        background.style.backgroundColor = "green";
+        shuttleHeight.innerHTML = "0";
+    })
+
 
 });
